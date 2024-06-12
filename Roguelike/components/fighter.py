4539897ed7dbcb3f96e398ea0eae76db9a64469e
jloomis92 <jack.loomis@pm.me>
2024-06-12 15:56:16 -0400

    Feature: Player can now properly die and the game will end.

--- Roguelike/components/fighter.py
+++ Roguelike/components/fighter.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 from components.base_component import BaseComponent
+from input_handlers import GameOverEventHandler
+from render_order import RenderOrder
 
 if TYPE_CHECKING:
     from entity import Actor
@@ -16,17 +18,18 @@ class Fighter(BaseComponent):
         
     @property
     def hp(self) -> int:
-        return self.hp
+        return self._hp
     
     @hp.setter
     def hp(self, value: int) -> None:
-        self.hp = max(0, min(value, self.max_hp))
+        self._hp = max(0, min(value, self.max_hp))
         if self._hp == 0 and self.entity.ai:
             self.die()
             
     def die(self) -> None:
         if self.engine.player is self.entity:
             death_message = "You died!"
+            self.engine.event_handler = GameOverEventHandler(self.engine)
         else:
             death_message = f"{self.entity.name} is dead!"
             
@@ -35,5 +38,6 @@ class Fighter(BaseComponent):
         self.entity.blocks_movement = False
         self.entity.ai = None
         self.entity.name = f"remains of {self.entity.name}"
+        self.entity.render_order = RenderOrder.CORPSE
         
         print(death_message)
