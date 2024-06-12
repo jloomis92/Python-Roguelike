882ebcf72579b92381519b9002c6c7076b990ad8
jloomis92 <jack.loomis@pm.me>
2024-06-12 09:59:13 -0400

    Feature: Added FOV so that the map is only visible to a certain point.

--- Roguelike/engine.py
+++ Roguelike/engine.py
@@ -1,6 +1,7 @@
 from typing import Set, Iterable, Any
 from tcod.context import Context
 from tcod.console import Console
+from tcod.map import compute_fov
 from entity import Entity
 from game_map import GameMap
 from input_handlers import EventHandler
@@ -11,6 +12,7 @@ class Engine:
         self.event_handler = event_handler
         self.player = player
         self.game_map = game_map
+        self.update_fov()
 
     def handle_events(self, events: Iterable[Any]) -> None:
         for event in events:
@@ -21,11 +23,25 @@ class Engine:
             
             action.perform(self, self.player)
             
+            self.update_fov() # Update the FOV before the players next action.
+            
+    def update_fov(self) -> None:
+        """Recompute the visible are based on the players point of view."""
+        self.game_map.visible[:] = compute_fov(
+            self.game_map.tiles["transparent"],
+            (self.player.x, self.player.y),
+            radius=8,
+        )
+        # If a tile is "visible" it should be added to "explored".
+        self.game_map.explored |= self.game_map.visible
+            
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
 
         for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, fg=entity.color)
+            # Only print entities that are in the FOV
+            if self.game_map.visible[entity.x, entity.y]:
+                console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
         context.present(console)
 
