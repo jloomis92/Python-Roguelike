46b05969c2315e05b60603415ac9176d3a3485d5
jloomis92 <jack.loomis@pm.me>
2024-06-12 09:13:11 -0400

    Added features to procgen which draws a tunnel between our two static rooms.

--- Roguelike/procgen.py
+++ Roguelike/procgen.py
@@ -1,5 +1,7 @@
-from typing import Tuple
+import random
+from typing import Iterator, Tuple
 from game_map import GameMap
+import tcod
 import tile_types
 
 class RectangularRoom:
@@ -21,6 +23,25 @@ class RectangularRoom:
         """Return the inner area of this room as a 2D array index."""
         return slice(self.x1 + 1, self.x2), slice(self.y1 + 1, self.y2)
     
+def tunnel_between(
+    start: Tuple[int, int], end: Tuple[int, int]
+    ) -> Iterator[Tuple[int, int]]:
+    """Return an L-shaped tunnel between these two points."""
+    x1, y1 = start
+    x2, y2 = end
+    if random.random() < 0.5: # 50% chance.
+        # Move horizontally, then vertically
+        corner_x, corner_y = x2, y1
+    else:
+        # Move vertically, then horizontally
+        corner_x, corner_y = x1, y2
+        
+    # Generate the coordinates for this tunnel.
+    for x, y in tcod.los.bresenham((x1,  y1), (corner_x, corner_y)).tolist():
+        yield x, y
+    for x, y in tcod.los.bresenham((corner_x, corner_y), (x2, y2)).tolist():
+        yield x, y
+  
 def generate_dungeon(map_width, map_height) -> GameMap:
     dungeon = GameMap(map_width, map_height)
 
@@ -29,5 +50,8 @@ def generate_dungeon(map_width, map_height) -> GameMap:
 
     dungeon.tiles[room_1.inner] = tile_types.floor
     dungeon.tiles[room_2.inner] = tile_types.floor
+    
+    for x, y in tunnel_between(room_2.center, room_1.center):
+        dungeon.tiles[x, y] = tile_types.floor
 
     return dungeon
