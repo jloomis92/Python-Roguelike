bd5565c25b5928de5925da74a2c98140b997355e
jloomis92 <jack.loomis@pm.me>
2024-06-12 14:59:16 -0400

    Adding in pathfinding for AI (monsters)

--- Roguelike/procgen.py
+++ Roguelike/procgen.py
@@ -7,7 +7,7 @@ import tcod
 import tile_types
 
 if TYPE_CHECKING:
-    from entity import Entity
+    from engine import Engine
 
 class RectangularRoom:
     def __init__(self, x: int, y: int, width: int, height: int):
@@ -78,10 +78,11 @@ def generate_dungeon(
     map_width: int,
     map_height: int,
     max_monsters_per_room: int,
-    player: Entity,
+    engine: Engine,
 ) -> GameMap:
     """Generate a new dungeon map."""
-    dungeon = GameMap(map_width, map_height, entities=[player])
+    player = engine.player
+    dungeon = GameMap(engine, map_width, map_height, entities=[player])
     
     rooms: List[RectangularRoom] = []
     
@@ -105,7 +106,7 @@ def generate_dungeon(
         
         if len(rooms) == 0:
             # The first room, where the player starts.
-            player.x, player.y = new_room.center
+            player.place(*new_room.center, dungeon)
         else: # All rooms after the first
             # Dig out a tunnel between this room and the previous one.
             for x, y in tunnel_between(rooms[-1].center, new_room.center):
