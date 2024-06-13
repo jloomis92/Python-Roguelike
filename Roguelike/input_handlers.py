974fb6c70c640c9568186b8ab94b75d596a71238
jloomis92 <jack.loomis@pm.me>
2024-06-13 01:12:11 -0400

    Finished save/load feature

--- Roguelike/input_handlers.py
+++ Roguelike/input_handlers.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import os
 from typing import Callable, Optional, Tuple, TYPE_CHECKING, Union
 import tcod.event
 import tcod
@@ -79,6 +80,32 @@ class BaseEventHandler(tcod.event.EventDispatch[ActionOrHandler]):
     
     def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
         raise SystemExit()
+    
+class PopupMessage(BaseEventHandler):
+    """Dislpay a popup text window."""
+    
+    def __init__(self, parent_handler: BaseEventHandler, text: str):
+        self.parent = parent_handler
+        self.text = text
+        
+    def on_render(self, console: tcod.Console) -> None:
+        """Render the parent and dim the result, then print the message on top."""
+        self.parent.on_render(console)
+        console.tiles_rgb["fg"] //= 8
+        console.tiles_rgb["bg"] //= 8
+        
+        console.print(
+            console.width // 2,
+            console.height // 2,
+            self.text,
+            fg=color.white,
+            bg=color.black,
+            alignment=tcod.CENTER,
+        )
+        
+    def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[BaseEventHandler]:
+        """Any key returns to the parent handler."""
+        return self.parent
 
 class EventHandler(BaseEventHandler):
     def __init__(self, engine: Engine):
@@ -380,10 +407,19 @@ class MainGameEventHandler(EventHandler):
         return action
     
 class GameOverEventHandler(EventHandler):
+    
+    def on_quit(self) -> None:
+        """Handle exiting out of a finished game."""
+        if os.path.exists("savegame.sav"):
+            os.remove("savegame.sav") # Deletes the active save file.
+        raise exceptions.QuitWithoutSaving() # Avoid saving a finished game.
+    
+    def ev_quit(self, event: tcod.event.Quit) -> None:
+        self.on_quit()
 
     def ev_keydown(self, event: tcod.event.KeyDown) -> None:
         if event.sym == tcod.event.K_ESCAPE:
-            raise SystemExit()
+            self.on_quit()
 
 CURSOR_Y_KEYS = {
     tcod.event.K_UP: -1,
--- Roguelike/setup_game.py
+++ Roguelike/setup_game.py
@@ -2,6 +2,9 @@
 from __future__ import annotations
 
 import copy
+import lzma
+import pickle
+import traceback
 from typing import Optional
 
 
@@ -53,6 +56,13 @@ def new_game() -> Engine:
     )
     return engine
 
+def load_game(filename: str) -> Engine:
+    """Load an Engine instance from a file."""
+    with open(filename, "rb") as f:
+        engine = pickle.loads(lzma.decompress(f.read()))
+    assert isinstance(engine, Engine)
+    return engine
+
 
 class MainMenu(input_handlers.BaseEventHandler):
     """Handle the main menu rendering and input."""
@@ -96,8 +106,13 @@ class MainMenu(input_handlers.BaseEventHandler):
         if event.sym in (tcod.event.K_q, tcod.event.K_ESCAPE):
             raise SystemExit()
         elif event.sym == tcod.event.K_c:
-            # TODO: Load the game here
-            pass
+            try:
+                return input_handlers.MainGameEventHandler(load_game("savegame.sav"))
+            except FileNotFoundError:
+                return input_handlers.PopupMessage(self, "No saved game to load.")
+            except Exception as exc:
+                traceback.print_exc() # Print to stderr.
+                return input_handlers.PopupMessage(self, f"Failed to load save:\n{exc}")
         elif event.sym == tcod.event.K_n:
             return input_handlers.MainGameEventHandler(new_game())
         
