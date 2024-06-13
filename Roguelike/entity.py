bb28a6064edede15d195e6f918b4601ab48d9be6
jloomis92 <jack.loomis@pm.me>
2024-06-12 22:41:33 -0400

    Feature: Implemented inventory system is comlpete and functional.

--- Roguelike/entity.py
+++ Roguelike/entity.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 import copy
-from typing import Optional, Tuple, Type, TypeVar, TYPE_CHECKING
+from typing import Optional, Tuple, Type, TypeVar, TYPE_CHECKING, Union
 from render_order import RenderOrder
 
 if TYPE_CHECKING:
     from components.ai import BaseAI
     from components.consumable import Consumable
     from components.fighter import Fighter
+    from components.inventory import Inventory
     from game_map import GameMap
 
 T = TypeVar("T", bound="Entity")
@@ -16,7 +17,7 @@ class Entity:
     A generic object to represent players, enemies, items, etc.
     """
     
-    parent: GameMap
+    parent: Union[GameMap, Inventory]
     
     def __init__(
         self,
@@ -80,7 +81,8 @@ class Actor(Entity):
         color: Tuple[int, int, int] = (255, 255, 255),
         name: str = "<Unnamed>",
         ai_cls: Type[BaseAI],
-        fighter: Fighter
+        fighter: Fighter,
+        inventory: Inventory,
     ):
         super().__init__(
             x=x,
@@ -96,6 +98,9 @@ class Actor(Entity):
         
         self.fighter = fighter
         self.fighter.parent = self
+        
+        self.inventory = inventory
+        self.inventory.parent = self
         
     @property
     def is_alive(self) -> bool:
