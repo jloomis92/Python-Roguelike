b2b4bbae3bb64b4df6146d2b72a00b90a3416112
jloomis92 <jack.loomis@pm.me>
2024-06-12 20:53:03 -0400

    Testing

--- Roguelike/main.py
+++ Roguelike/main.py
@@ -2,6 +2,7 @@
 import tcod
 import color
 import copy
+import traceback
 from engine import Engine
 import entity_factories
 from procgen import generate_dungeon
@@ -55,8 +56,15 @@ def main() -> None:
             root_console.clear()
             engine.event_handler.on_render(console=root_console)
             context.present(root_console)
-
-            engine.event_handler.handle_events(context)
+            
+            try:
+                for event in tcod.event.wait():
+                    context.convert_event(event)
+                    engine.event_handler.handle_events(event)
+            except Exception: # Handle exceptions in game.
+                traceback.print_exc() # Print error to stderr.
+                # Then print the error to the message log.
+                engine.message_log.add_message(traceback.format_exc(), color.error)
 
 if __name__ == "__main__":
     main()
