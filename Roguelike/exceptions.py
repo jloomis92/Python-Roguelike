23685791773e8211ccb866883fd1d708dc322d6b
jloomis92 <jack.loomis@pm.me>
2024-06-12 20:59:04 -0400

    Added try/except clauses when players/ai do things that are impossible.

--- Roguelike/exceptions.py
+++ Roguelike/exceptions.py
@@ -1,4 +1,4 @@
-class Impossible(Excpetion):
+class Impossible(Exception):
     """Exception raised when an action is impossible to be performed.
     
     The reason is given as the exception message.
